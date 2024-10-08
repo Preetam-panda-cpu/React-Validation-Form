deaa5eb5a60557e7a1e13a4417035ce51ebe783f
Preetam Panda <64851193+Preetam-panda-cpu@users.noreply.github.com>
2024-10-08 16:53:54 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+          <><div>
+            <header><h1>Contact Us Form</h1></header>
+          </div>
+          <div class="container">
+              <form>
+              <label for="fname">First Name</label>
+              <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
+              <label for="fname">Last Name</label>
+              <input type="text" id="fname" name="Lastname" placeholder="Your name.."></input>
+              <label for="country">Gender</label>
+    <div class="gender">
+      <select id="gender" name="gender">
+      <option value="Male">Male</option>
+      <option value="Female">Female</option>
+      <option value="Others">Others</option>
+    </select></div>
+              <label for="subject">Comments</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
+ <button onclick="myFunction()">Submit</button>
+              </form>
+            </div></>
+        );
+      }
+    }
+    
+    export default App;
